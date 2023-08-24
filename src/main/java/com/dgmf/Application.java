4b7d84531888d0e83c1536b31e472cbeefaf9013
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2023-08-24 13:02:21 +0200

    Spring Data JPA - Pagination Implementation

--- src/main/java/com/dgmf/Application.java
+++ src/main/java/com/dgmf/Application.java
@@ -1,29 +1,24 @@
 package com.dgmf;
 
-import com.dgmf.entity.Product;
-import com.dgmf.repository.ProductRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.math.BigDecimal;
-
 @SpringBootApplication
-public class Application implements CommandLineRunner {
-	@Autowired
-	private ProductRepository productRepository;
+public class Application // implements CommandLineRunner
+{
+	/*@Autowired
+	private ProductRepository productRepository;*/
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
 
-	@Override
+	/*@Override
 	public void run(String... args) throws Exception {
 		Long productPrice = 0L;
 
 		for(int i = 1; i <= 100; i++) {
-			productPrice += 100L;
+			productPrice += 10L;
 
 			Product product = Product.builder()
 					.productName("Product " + i)
@@ -36,5 +31,5 @@ public class Application implements CommandLineRunner {
 
 			productRepository.save(product);
 		}
-	}
+	}*/
 }
--- src/test/java/com/dgmf/repository/PaginationAndSortingTest.java
+++ src/test/java/com/dgmf/repository/PaginationAndSortingTest.java
@@ -1,12 +1,65 @@
 package com.dgmf.repository;
 
+import com.dgmf.entity.Product;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
 
 @SpringBootTest
 public class PaginationAndSortingTest {
     @Autowired
     private ProductRepository productRepository;
 
+    @Test
+    void pagination() {
+        // Typically, "pageNo" and "pageSize" come from the Client
+        // Page number ==> Spring Data JPA starts
+        // the count of Pages to 0
+        int pageNo = 0;
+        // Number of Products displayed per Page ==> Here,
+        // 5 Products per Page
+        int pageSize = 5;
 
+        // Creates a "pageable" Object
+        // from ==> "org.springframework.data.domain"
+        Pageable pageable = PageRequest.of(pageNo, pageSize);
+        // A Page of Products
+        Page<Product> page = productRepository.findAll(pageable);
+        // A List of Products from "page" Object
+        List<Product> products = page.getContent();
+        // Displays the Products (5 products per page)
+        products.forEach(product ->
+                        System.out.println(product)
+                );
+
+        // Information about the Page
+        // Total Pages
+        int totalPages = page.getTotalPages();
+        // Total elements
+        Long totalElements = page.getTotalElements();
+        // Number of elements per Page
+        int numberOfElementsPerPage = page.getNumberOfElements();
+        // Size of the Page
+        int size = page.getSize();
+        // Is it the First Page ?
+        boolean isFirst = page.isFirst();
+        // Is it the Last Page ?
+        boolean isLast = page.isLast();
+
+        // Displays Information about the Page
+        System.out.println(
+                "Total Pages : " + totalPages + "\n" +
+                "Total elements : " + totalElements + "\n" +
+                "Number of elements per Page : " + numberOfElementsPerPage + "\n" +
+                "Size of the Page : " + size + "\n" +
+                "Is it the First Page ? " + isFirst + "\n" +
+                "Is it the Last Page ? " + isLast + "\n"
+        );
+
+    }
 }
