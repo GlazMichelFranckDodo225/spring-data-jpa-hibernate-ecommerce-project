2a7de5e3c1ae73df1308f1af1ccc8620d46ada06
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2023-09-05 16:45:10 +0200

    Create OrderRepository and AddressRepository

---
+++ src/main/java/com/dgmf/repository/OrderRepository.java
@@ -0,0 +1,8 @@
+package com.dgmf.repository;
+
+import com.dgmf.entity.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+    Order findByOrderTrackingNumber(String orderTrackingNumber);
+}
