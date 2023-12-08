d59bd4560c895f617d415da34128772110b91402
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2023-12-08 10:13:15 +0100

    Transaction Management with Spring Data JPA

--- src/main/java/com/dgmf/repository/OrderRepository.java
+++ src/main/java/com/dgmf/repository/OrderRepository.java
@@ -2,7 +2,17 @@ package com.dgmf.repository;
 
 import com.dgmf.entity.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
     Order findByOrderTrackingNumber(String orderTrackingNumber);
+    // To Override Default Transactional Settings for JpaRepository Method
+    // org.springframework.transaction.annotation.Transactional
+    @Override
+    @Transactional(timeout = 10)
+    void deleteById(Long aLong);
+    @Override
+    @Transactional(propagation = Propagation.NEVER)
+    <S extends Order> S save(S entity);
 }
--- src/main/java/com/dgmf/repository/ProductRepository.java
+++ src/main/java/com/dgmf/repository/ProductRepository.java
@@ -4,8 +4,6 @@ import com.dgmf.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-
-import java.lang.annotation.Native;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.List;
